0897c2aebb049d28337f38eb06654d7d1b1e446d
algoritmika <algo@mail.ru>
2024-05-05 17:09:14 +0300

    tg123

--- b.py
+++ b.py
@@ -1,3 +1,8 @@
 print("fre nggrs be able well")
 a = "ne strelyai"
 b = "strelyai"
+@bot.callback_query_handler(func=lambda call: True)
+def callback_worker(call):
+    if call.data == "zodiac": 
+        msg = random.choice(first) + ' ' + random.choice(second) + ' ' + random.choice(second_add) + ' ' + random.choice(third)
+        bot.send_message(call.message.chat.id, msg)
