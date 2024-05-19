6462dff87ea2a502e67b07c9ff5ac2c50d36cefa
Your Name <you@example.com>
2024-05-19 18:52:46 +0300

    tg13377:

--- d.py
+++ d.py
@@ -15,7 +15,7 @@ def get_words():
         d = json.load(file)
     return d       
 a = get_words()
-
+users = {}
 
 bot = telebot.TeleBot(get_token())         
 @bot.message_handler(content_types=['text']) 
@@ -25,17 +25,26 @@ def get_text_messages(message):
         keyboard = types.InlineKeyboardMarkup()
          
         for i in a.keys():
+            keyboard.add(types.InlineKeyboardButton(text=i, callback_data="cat"+i) ) 
             
-            keyboard.add(types.InlineKeyboardButton(text=i,callback_data=i) ) 
         
-        bot.send_message(message.from_user.id, text='Выбери о какой команде хочешь узнать ', reply_markup=keyboard) 
+        bot.send_message(message.from_user.id, text='Выбери о какой категории хочешь узнать ', reply_markup=keyboard) 
     elif message.text == "/help": 
         bot.send_message(message.from_user.id, "Напиши Привет") 
     else: 
         bot.send_message(message.from_user.id, "Напиши /help.") 
 @bot.callback_query_handler(func=lambda call: True) 
 def callback_worker(call): 
-    bot.send_message(call.from_user.id, a[call.data]) 
+    if call.data.find('cat') == 0:
+        users[call.from_user.id] = call.data.replace('cat', '')
+        keyboard = types.InlineKeyboardMarkup()
+         
+        for i in a[users[call.from_user.id]].keys():
+            keyboard.add(types.InlineKeyboardButton(text=i, callback_data=i) ) 
+            
+        bot.send_message(call.from_user.id, text='Выбери о какой команде хочешь узнать ', reply_markup=keyboard) 
+    else:
+        bot.send_message(call.from_user.id, a[users[call.from_user.id]][call.data]) 
 
 bot.polling(none_stop=True, interval=0)
 
